253f710bfbb0ff24ea234048519d767411396988
Francol11 <francominelli11@gmail.com>
2024-10-02 22:45:32 -0300

    Mejoras en la app

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,18 @@
-INSERT INTO CLIENTES VALUES
-(1, 'Tito', 'Calderón', 12345678),
-(2, 'Rusty', 'Curly', 91011121),
-(3, 'Mico', 'Lawrie', 31415161);
+INSERT INTO CLIENTES (name,lastname,dni) VALUES
+('Tito', 'Calderón', 12345678),
+('Rusty', 'Curly', 91011121),
+('Mico', 'Lawrie', 31415161);
 
-INSERT INTO PRODUCTOS VALUES
-(1717, 'Fideos MAROLIO', 'Marolio', 3, 1200),
-(6534, 'Galletas DIVERSION', 'Diversión', 4, 2500),
-(3782, 'Gaseosa SPRITE', 'Sprite', 5, 3000);
+INSERT INTO PRODUCTOS (description,code,stock,price) VALUES
+('Fideos MAROLIO', 'Marolio', 3, 1200),
+('Galletas DIVERSION', 'Diversión', 4, 2500),
+('Gaseosa SPRITE', 'Sprite', 5, 3000);
 
-INSERT INTO INVOICE VALUES
-(1, 12, 'Tito Calderón', 50.00),
-(2, 13, 'Rusty Curly', 10.20),
-(3, 14, 'Mico Lawrie', 74.89);
+INSERT INTO INVOICE (client_id,total) VALUES
+(1, 50.00),
+(2, 10.20),
+(3, 74.89);
 
-INSERT INTO INVOICE_DETAILS VALUES
-(1,12,5000,34,234.55),
-(2,13,6465,343,678.88);
+INSERT INTO INVOICE_DETAILS (invoice_id,amount,product_id,price) VALUES
+(1, 5000, 1, 234.55),
+(2, 6465, 2, 678.88);
